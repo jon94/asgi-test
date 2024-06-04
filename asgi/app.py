ae19d7f95ad47f6e1237a610ec63a7b0a376da9a
jon94 <jonathan.lim@datadoghq.com>
2024-06-04 17:51:49 +0800

    tried asgi with ddtracer does not show spans, but shows apm metrics

--- asgi/app.py
+++ asgi/app.py
@@ -1,14 +1,12 @@
-# Filename: app.py
-# pip install Quart==0.19.4 ddtrace==2.6.3 Hypercorn==0.16.0
-# Run script with (on python3.9): DD_SERVICE=monkey QUART_APP=test:app1 ddtrace-run hypercorn -b 127.0.0.1:8000 test:app
-# Navigate to http://localhost:8001/hello in a browser to send a trace
 from quart import Quart
 from ddtrace.contrib.asgi import TraceMiddleware
 
-app1 = Quart(__name__)
+app = Quart(__name__)
+app.asgi_app = TraceMiddleware(app.asgi_app)
 
-@app1.route('/hello')
+@app.route("/")
 async def hello():
-    return 'hello'
+    return "Hello!"
 
-app = TraceMiddleware(app1)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
